c815efb8e25711e1fdb02fff145242745bbf13b0
Angel <angelclaudio@outlook.com>
2023-11-09 19:42:12 -0500

    Chapter 1 Complete

--- Chapter_01/BooksStore/BooksStore/Program.cs
+++ Chapter_01/BooksStore/BooksStore/Program.cs
@@ -7,8 +7,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// WebAssembyHostBuilder.Services.AddScoped method is the DI container where you register your services.
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+// In the below case, the ConsoleLoggingService implementation, is registered for ILoggingService
 builder.Services.AddScoped<ILoggingService, ConsoleLoggingService>();
 
 await builder.Build().RunAsync();
